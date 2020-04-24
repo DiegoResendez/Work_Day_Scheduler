7955f75adabdb2347fba8781275b4a9ba57b2a7d
DiegoResendez <61667616+DiegoResendez@users.noreply.github.com>
2020-04-24 16:05:42 -0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,17 @@
+const timePlaceHolder = document.getElementById("timePlaceHolder")
+
+
+
+
+
+
+// For loop for the time - Need help with this
+
+timePlaceHolder["9:00 AM" "10:00 AM" "11:00 AM" "12:00 PM" "1:00 PM" "2:00 PM" "3:00 PM" "4:00 PM" "5:00 PM"]
+
+let i = timePlaceHolder;
+
+for (let i = 0; i < timePlaceHolder.length; i++){
+    $("<a>").text[i];
+    // should add the input box after each time if possible
+}
