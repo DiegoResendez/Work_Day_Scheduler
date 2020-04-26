919274c789c1b051a38afb6da543986e03946990
unknown <diego.resendez@yahoo.com>
2020-04-26 14:27:08 -0700

    Sunday push

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 const timePlaceHolder = document.getElementById("timePlaceHolder")
-
+const saveButton = document.getElementById("saveButton");
 
 
 
@@ -9,9 +9,23 @@ const timePlaceHolder = document.getElementById("timePlaceHolder")
 
 timePlaceHolder["9:00 AM" "10:00 AM" "11:00 AM" "12:00 PM" "1:00 PM" "2:00 PM" "3:00 PM" "4:00 PM" "5:00 PM"]
 
-let i = timePlaceHolder;
 
-for (let i = 0; i < timePlaceHolder.length; i++){
-    $("<a>").text[i];
+
+    let time = $("<a>");
+
+    for (let i = 0; i < timePlaceHolder.length; i++){
+    timePlaceHolder = [i]; 
+        let timestamp = document.getElementById(`eventSpan${i}`)
+        // document.getElementById("eventSpan" + i)
+        timestamp.textContent = timePlaceHolder[i];
+    $(time.timePlaceHolder)
     // should add the input box after each time if possible
-}
+    }
+
+//     .addEventListener("click", function(event) {
+//         event.preventDefault();
+      
+//         console.log(event);
+      
+// }
+
