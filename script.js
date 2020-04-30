6edd03c6f95316fed963d67e1d7799f108a07951
unknown <diego.resendez@yahoo.com>
2020-04-29 21:46:57 -0700

    Wednesday night upload

--- script.js
+++ script.js
@@ -1,26 +1,87 @@
-const timePlaceHolder = document.getElementById("timePlaceHolder")
-const saveButton = document.getElementById("saveButton");
+const timeDisplayElem = document.getElementById("timeDisplay");
+const saveButton = document.getElementById("save-btn");
+const textInput = document.getElementById("textInput");
+const timeStamp = document.getElementById("timeStamp");
+const userInput = document.getElementById("eventID3");
+let button8AM = document.getElementById("8AMsave-btn")
+
+// let eventSpan11 = 6:00 PM;
+// let eventSpan10 = 5:00 PM;
+// let eventSpan9 = 4:00 PM;
+// let eventSpan8 = 3:00 PM;
+// let eventSpan7 = 2:00 PM;
+// let eventSpan6 = 1:00 PM;
+// let eventSpan5 = 12:00 PM;
+// let eventSpan4 = 11:00 AM;
+// let eventSpan3 = 10:00 AM;
+// let eventSpan2 = 9:00 AM;
+// let eventSpan1 = 8:00 AM;
+
+let time = timeStamp;
+let textInput;
+button8AM.addEventListener("click", saveTextInput);
+// document.getElementById('btn').addEventListener("click", saveToDo); 
+
+saveButton.addEventListener("click", );
+
+
+function setBackgroundColor(){
+
+    
+}console.log("save click")
 
 
 
+let todos = [];
 
+function saveToDo = (ev)=>{
+    ev.preventDefault();
+    let todo = {
+        id: Date.now(),
+        activity: document.getElementById("textInput").textContent
+                } 
+                    console.log(textContent)
+    todos.push(todo);
 
-// For loop for the time - Need help with this
+    //for display purposes only
+    console.warn('added', {todos});
+    let pre = document.querySelector('#save-btn')
+};
 
-timePlaceHolder["9:00 AM" "10:00 AM" "11:00 AM" "12:00 PM" "1:00 PM" "2:00 PM" "3:00 PM" "4:00 PM" "5:00 PM"]
+function saveTextInput(){
 
+    function saveToDo(){
 
-
-    let time = $("<a>");
-
-    for (let i = 0; i < timePlaceHolder.length; i++){
-    timePlaceHolder = [i]; 
-        let timestamp = document.getElementById(`eventSpan${i}`)
-        // document.getElementById("eventSpan" + i)
-        timestamp.textContent = timePlaceHolder[i];
-    $(time.timePlaceHolder)
-    // should add the input box after each time if possible
     }
+
+    console.log("saveTextInput")
+
+    //save to local storage
+    localStorage.setItem("#textInput", JSON.stringify(todos));
+
+}
+    // document.addEventListener('DOMContentLoaded', ()=>{
+
+
+
+
+
+// saveButton.addEventListener("click", saveTextInput)
+
+
+// let date = new Date();
+// document.write(date);
+
+
+// function saveTextInput(){
+//     console.log("save click");
+
+//     //add textInput to local storage and persist
+
+// }
+
+
+    // should add the input box after each time if possible
 
 //     .addEventListener("click", function(event) {
 //         event.preventDefault();
@@ -28,4 +89,6 @@ timePlaceHolder["9:00 AM" "10:00 AM" "11:00 AM" "12:00 PM" "1:00 PM" "2:00 PM" "
 //         console.log(event);
       
 // }
+
+
 
