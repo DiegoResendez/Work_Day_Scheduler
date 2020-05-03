755edbcb45b74cfa1d367dad83547157bdffd8f3
unknown <diego.resendez@yahoo.com>
2020-05-02 20:52:18 -0700

    Saturday Night Upload

--- script.js
+++ script.js
@@ -1,9 +1,11 @@
+
+
+
 const timeDisplayElem = document.getElementById("timeDisplay");
-const saveButton = document.getElementById("save-btn");
 const textInput = document.getElementById("textInput");
 const timeStamp = document.getElementById("timeStamp");
 const userInput = document.getElementById("eventID3");
-let button8AM = document.getElementById("8AMsave-btn")
+const button8AM = document.getElementById("8AMsave-btn")
 
 // let eventSpan11 = 6:00 PM;
 // let eventSpan10 = 5:00 PM;
@@ -17,53 +19,52 @@ let button8AM = document.getElementById("8AMsave-btn")
 // let eventSpan2 = 9:00 AM;
 // let eventSpan1 = 8:00 AM;
 
-let time = timeStamp;
-let textInput;
-button8AM.addEventListener("click", saveTextInput);
-// document.getElementById('btn').addEventListener("click", saveToDo); 
+// let time = timeStamp;
+// let textInput;
+button8AM.addEventListener("click", saveActivity);
 
-saveButton.addEventListener("click", );
-
-
-function setBackgroundColor(){
-
+function saveActivity(){
+    // event.preventDefault()
+    console.log("save click")
     
-}console.log("save click")
-
-
-
-let todos = [];
-
-function saveToDo = (ev)=>{
-    ev.preventDefault();
-    let todo = {
-        id: Date.now(),
-        activity: document.getElementById("textInput").textContent
-                } 
-                    console.log(textContent)
-    todos.push(todo);
-
-    //for display purposes only
-    console.warn('added', {todos});
-    let pre = document.querySelector('#save-btn')
-};
-
-function saveTextInput(){
-
-    function saveToDo(){
-
-    }
-
-    console.log("saveTextInput")
-
-    //save to local storage
-    localStorage.setItem("#textInput", JSON.stringify(todos));
-
 }
-    // document.addEventListener('DOMContentLoaded', ()=>{
+// document.getElementById('8AMsave-btn').addEventListener("click", saveActivity); 
 
 
+// let todos = [];
 
+// function saveToDo = (ev)=>{
+//     ev.preventDefault();
+//     let todo = {
+//         id: Date.now(),
+//         activity: document.getElementById("textInput").textContent
+//     } 
+//     console.log(textContent)
+//     todos.push(todo);
+    
+//     //for display purposes only
+//     console.warn('added', {todos});
+//     let pre = document.querySelector('#save-btn')
+// };
+
+// function saveTextInput(){
+    
+//     function saveToDo(){
+        
+//     }
+    
+//     console.log("saveTextInput")
+    
+    //save to local storage
+//     localStorage.setItem("#textInput", JSON.stringify(todos));
+    
+// }
+// document.addEventListener('DOMContentLoaded', ()=>{
+    
+    
+    
+    // function setBackgroundColor(){
+    // }console.log("save click")
 
 
 // saveButton.addEventListener("click", saveTextInput)
